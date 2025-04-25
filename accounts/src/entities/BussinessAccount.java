5086d4f4181307a78f55a5988688a0b9f63cfa5f
Rodrigo <cabralrodrigo787@gmail.com>
2025-04-24 20:23:30 -0400

    Primerio commit

---
+++ accounts/src/entities/BussinessAccount.java
@@ -0,0 +1,32 @@
+package entities;
+
+public class BussinessAccount extends Account{
+    public Double loanLimit;
+
+    BussinessAccount(){}
+
+    public BussinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+        super(number, holder, balance);
+        this.loanLimit = loanLimit;
+    }
+
+    public Double getLoanLimit() {
+        return loanLimit;
+    }
+
+    public void setLoanLimit(Double loanLimit) {
+        this.loanLimit = loanLimit;
+    }
+
+    public void loan(Double amount){
+        if (amount<= loanLimit){
+            balance+= amount -10.0;
+        }
+    }
+
+    @Override
+    public void withdraw(Double amount){
+        super.withdraw(amount);
+        balance -= 2.0;
+    }
+}
